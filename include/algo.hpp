b9fe9e7e11b7945f004134a9ac94e02c8847d407
Naman Gupta <namangupta98@J.A.R.V.I.S>
2019-11-18 14:16:46 -0500

    [include][src]Added doxygen and inline comments

--- include/algo.hpp
+++ include/algo.hpp
@@ -1,3 +1,42 @@
+/**
+ * Copyright (c) 2019, Naman Gupta
+ *
+ * Redistribution and use in source and binary forms, with or without  
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the   
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ *@file       algo.hpp
+ *@author     Naman Gupta
+ *@copyright  GNU
+ *@brief      Class to implement obstacle avoidance algorithm
+ */
+
 #ifndef INCLUDE_ALGORITHM_HPP
 #define INCLUDE_ALGORITHM_HPP
 
@@ -30,22 +69,26 @@ public:
  	 * @brief 	Constructor for objects
  	 */
 	Algo();
+
 	/**
 	 * @brief	Destructor for objects
 	 */
 	~Algo();
+
 	/**
  	 * @brief 	function to callback laser scanner
  	 * @return 	none
  	 * @param	stat pointer which stores scanner status of type constant
  	 */
 	void laserScan(const sensor_msgs::LaserScan::ConstPtr& stat);
+
 	/**
 	 * @brief	function which tells that wall detected or not
 	 * @param	none
 	 * @return	returns true if wall detected else false if not of type bool
 	 */
 	bool obstacle();
+
 	/**
 	 * @brief	function to move the robot
 	 * @param	none
--- src/algo.cpp
+++ src/algo.cpp
@@ -1,3 +1,42 @@
+/**
+ * Copyright (c) 2019, Naman Gupta
+ *
+ * Redistribution and use in source and binary forms, with or without  
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the   
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ *@file       algo.cpp
+ *@author     Naman Gupta
+ *@copyright  GNU
+ *@brief      Implementation of obstacle avoidance class
+ */
+
 #include <iostream>
 #include "algo.hpp"
 
@@ -56,6 +95,7 @@ bool Algo::obstacle() {
  */
 void Algo::laserScan(const sensor_msgs::LaserScan::ConstPtr& stat) {
   int i = 0;
+  // Looking for obstacle
   while (i < stat->ranges.size()) {
     if (stat->ranges[i] < 0.8) {
       wall = true;
@@ -75,7 +115,7 @@ void Algo::movement() {
   // Frequency 10 Hz
   ros::Rate loop(10);
   while (ros::ok()) {
-    // check wall
+    // Case for wall status true
     if (obstacle()) {
       // Wall detected
       ROS_INFO_STREAM("Oh, wall detected!! Turning anti-clockwise");
